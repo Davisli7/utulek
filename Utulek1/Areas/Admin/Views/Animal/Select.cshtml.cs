c7d0a61c516dbb4c60e0662139a4e4d431e850a3
s_svoboda <s_svoboda@utb.cz>
2025-10-30 11:16:43 +0100

    Admin Areas

---
+++ Utulek1/Areas/Admin/Views/Animal/Select.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Utulek1.Areas.Admin.Views.Product
+{
+    public class SelectModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
