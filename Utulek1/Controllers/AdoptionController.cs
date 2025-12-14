54124b2fc0388c92cc4c000d706962cfc31143c7
s_svoboda <s_svoboda@utb.cz>
2025-12-14 21:37:48 +0100

    Oddělení nabídky zvířat do vlastního view

---
+++ Utulek1/Controllers/AdoptionController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Utulek1.Application.Abstraction;
+using Utulek1.Domain.Entities;
+
+namespace Utulek1.Controllers
+{
+    public class AdoptionController : Controller
+    {
+        private readonly IAnimalAppService _animalAppService;
+
+        public AdoptionController(IAnimalAppService animalAppService)
+        {
+            _animalAppService = animalAppService;
+        }
+
+        public IActionResult Index()
+        {
+            // Načteme všechna zvířata (včetně fotek díky tvé úpravě v Service)
+            IList<Animal> animals = _animalAppService.Select();
+            return View(animals);
+        }
+    }
+}
